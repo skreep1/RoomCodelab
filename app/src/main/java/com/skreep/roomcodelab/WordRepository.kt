42d542f1465c6c4a12813488399890a801b774ca
skreep1 <skrypka.m.l@gmail.com>
2021-11-26 14:58:20 +0200

    Create Repository

---
+++ app/src/main/java/com/skreep/roomcodelab/WordRepository.kt
@@ -0,0 +1,19 @@
+package com.skreep.roomcodelab
+
+import androidx.annotation.WorkerThread
+import kotlinx.coroutines.flow.Flow
+
+class WordRepository {
+
+    class WordRepository(private val wordDao: WordDao) {
+
+
+        val allWords: Flow<List<Word>> = wordDao.getAlphabetizedWords()
+
+        @Suppress("RedundantSuspendModifier")
+        @WorkerThread
+        suspend fun insert(word: Word) {
+            wordDao.insert(word)
+        }
+    }
+}
