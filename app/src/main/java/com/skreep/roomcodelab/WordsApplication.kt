12aa687e242370f57737118969b031d656f72723
skreep1 <skrypka.m.l@gmail.com>
2021-11-27 17:17:48 +0200

    Create the repository and the database

---
+++ app/src/main/java/com/skreep/roomcodelab/WordsApplication.kt
@@ -0,0 +1,9 @@
+package com.skreep.roomcodelab
+
+import android.app.Application
+
+class WordsApplication: Application() {
+
+    val database by lazy { WordRoomDatabase.getDatabase(this) }
+    val repository by lazy { WordRepository(database.wordDao()) }
+}
