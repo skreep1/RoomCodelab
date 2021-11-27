aa8021dad21f3df268cec1d03f60d02110777de3
skreep1 <skrypka.m.l@gmail.com>
2021-11-27 17:30:26 +0200

    Populate the database

--- app/src/main/java/com/skreep/roomcodelab/WordsApplication.kt
+++ app/src/main/java/com/skreep/roomcodelab/WordsApplication.kt
@@ -1,9 +1,11 @@
 package com.skreep.roomcodelab
 
 import android.app.Application
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
 
-class WordsApplication: Application() {
-
-    val database by lazy { WordRoomDatabase.getDatabase(this) }
+class WordsApplication : Application() {
+    val applicationScope = CoroutineScope(SupervisorJob())
+    val database by lazy { WordRoomDatabase.getDatabase(this, applicationScope) }
     val repository by lazy { WordRepository(database.wordDao()) }
 }
